8bead8690bb9c6f39d4aff17dd04179f6565ec39
tuckerj0 <jat134@pitt.edu>
2016-10-13 21:49:04 -0400

    10/11

--- JSScripts/tabScript.js
+++ JSScripts/tabScript.js
@@ -14,3 +14,31 @@ function openPage(evt, pageName) {
     document.getElementById(pageName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+function validateForm(){
+
+    var x = document.forms["myForm"]["Name"].value;
+    if(x == null || x == ""){
+        alert("Name must be filled out");
+        return false;
+    }
+    var y = document.forms["myForm"]["Email"].value;
+    if(y == null || y == ""){
+        alert("Email must be filled out");
+        return false;
+    }
+    var z = document.forms["myForm"]["Subject"].value;
+    if(z == null || z == ""){
+         alert("Subject must be filled out");
+         return false;
+    }
+    var w = document.forms["myForm"]["Email"].value;
+    if(w == null || w == ""){
+        alert("Message is empty");
+        return false;
+    }
+    var form = document.getElementById("hide");
+    form.style.display = 'none';
+    var thanks = document.getElementById("show");
+    thanks.style.display = 'block';
+    return false;
+}
