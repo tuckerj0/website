7913876e0d45b04d5a2e5edbc84e28b163ddac0b
tuckerj0 <jat134@pitt.edu>
2016-12-02 18:14:11 -0500

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author: tuckerj0" content="">
+	<link rel="icon" type="image/png" href="images/letterj.png">
+    <title>Jason Tucker</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- Theme CSS -->
+    <link href="css/mystyle1.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#Experience">Experience</a>
+                    </li>
+					<li>
+                        <a class="page-scroll" href="#Abilities">Abilities</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">Jason Tucker</h1>
+                        <p class="intro-text">Resume</p>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2 class = "sectionTitle">Profile</h2>
+                <p class ="subTitle"> I'm a computer science student</p>
+				<div>
+					<div class = "leftpanel">
+						<p class = "head">About me</p>
+						<p>some stuff about mesome stuff aboutuff aboutsome stsome stuff about mesome stuff about meuff about mesome stuff about mesome stuff about mesome stuff about me meso stuff abouut mesome stuff about mesome stuff about mesome stuff about me</p>
+					</div>
+					<div class = "middlepanel">
+						<div class="picCont">
+							<img class ="myPic" src = "images/me.jpg">
+						</div>
+					</div>
+					<div class = "rightpanel">
+						<p class ="head">Details</p>
+						<p class = "bold">Name: </p>
+						<p>Jason Tucker</p>
+						<p class = "bold"> Age: </p>
+						<p>22 years</p>
+						<p class ="bold">Location: </p>
+						<p>Pittsburgh, Pennsylvania, US</p>
+					</div>
+				<div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Download Section -->
+    <section id="Experience" class="content-section text-center">
+		<div class="tanrow">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<h2 class = "tst">Experiences</h2>
+					<p class ="subTitle"> Some stuff I've done</p>
+					<div class = "exp">
+						<div class="expleft">
+							<p class = "exphead">Education</p>
+						</div>
+							<div class="expright"></div>
+					</div>
+					<div class="exp">
+						<div class = "expleft">
+							<p class = "place">University of Pittsburgh</p>
+							<p>2012-2017</p>
+						</div>
+						<div class = "expright">
+							<p class="expbold">Bachelor of Science - Computer Science</p>
+							<p>some stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about thissome stuff about this</p>
+						</div>
+					</div>
+					<div class = "border"></div>
+					<div class = "jobs">
+					<div class = "exp">
+						<div class="expleft">
+							<p class = "exphead">Jobs</p>
+						</div>
+							<div class="expright"></div>
+					</div>
+					<div class="exp">
+						<div class = "expleft">
+							<p class = "place">UBS</p>
+							<p>Financial Services</p>
+							<p>June 2016 - August 2016</p>
+						</div>
+						<div class = "expright">
+						<p class="expbold">Internship - Software Engineer</p>
+						<p>some stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuffsome stuff</p>
+						</div>
+					</div>
+					</div>
+				</div>
+			</div>
+        </div>
+    </section>
+	<section id="Abilities" class="content-section text-center">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2">
+				<h2 class = "sectionTitle">Abilities</h2>
+				<p class ="subTitle"> what can i do</p>
+			</div>
+		</div>
+	</section>
+    <!-- Contact Section -->
+    <section id="contact" class="content-section text-center">
+		<div class = "tanrow">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<h2 class = "tst">Contact</h2>
+					<p class ="subTitle"> contact stuff will be here</p>
+					<div>
+						<ul class="list-inline banner-social-buttons">
+							<li>
+								<a href="https://www.linkedin.com/in/jason-tucker-840412b4?trk=nav_responsive_tab_profile_pic" class="btn btn-default btn-lg"> <span class="network-name">Linkedin</span></a>
+							</li>
+							<li>
+								<a href="downloads/JasonTuckerResume.docx" download="JasonTuckerResume.docx" class="btn btn-default btn-lg">Download Resume</a>
+							</li>
+							<li>
+								<a href="https://github.com/tuckerj0" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+							</li>
+						</ul>
+					</div>
+					<div>
+					<section class = "show">
+						<form name = "myForm" id = "myForm" class = "contactForm" action = "">
+						<div class = "allform">
+							<div class = "contact">
+								<label class = "clabel">Name</label>
+								<input class = "cform" type = "text" name = "name" id = "name">
+							</div>
+							<div class = "contact">
+								<label class = "clabel">Email</label>
+								<input class = "cform" type = "Email" name = "email" id = "email">
+							</div>
+							<div class = "contactTA">							
+								<label class = "clabel">Message</label>
+								<textarea class = "cform" type = "text" name = "message" id = "message"></textarea>
+							</div>
+							</div>
+							<button class="btn btn-default btn-lg" type = "submit" id = "submit" form = "myForm">Submit</button>
+						</form>
+					</section>
+					<section class = "hidden">Thank you!</section>
+					</div>
+				</div>
+			</div>
+		</div>
+    </section>
+
+    <!-- Map Section -->
+    
+
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; Jason Tucker 2016</p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.js"></script>
+	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.js"></script>
+	<script src="http://ajax.microsoft.com/ajax/jquery.validate/1.11.1/additional-methods.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
